cf3caf0cc505e46600686ef2c6f2febf96e7fecb
Vinayak Anand <vinayakanand010503@gmail.com>
2025-10-25 13:36:59 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vinayak",
+    author_email="vinayakanand010503@gmail.com",
+    packages=find_packages()
+)
